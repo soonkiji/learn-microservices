c1bf459430cdd22f2d8bddb805506d343ae461a9
Soonki Ji <soonki.ji@gmail.com>
2017-02-15 22:46:14 -0500

    added springhateoas

---
+++ chap2/springhateoas/README.md
@@ -0,0 +1,19 @@
+# spring boot (hateoas)
+* mvnw clean install
+* java -jar target\springhateoas-0.0.1-SNAPSHOT.jar
+
+# controller endpoint
+* http://localhost:8080/
+* http://localhost:8080/greeting
+* http://localhost:8080/greeting?name=World
+
+# spring boot actuator endpoints
+* http://localhost:8080/env
+* http://localhost:8080/autoconfig
+* http://localhost:8080/metrics
+* http://localhost:8080/health
+* http://localhost:8080/info
+* http://localhost:8080/mappings
+* http://localhost:8080/beans
+* http://localhost:8080/dump
+* http://localhost:8080/trace
---
+++ chap2/springhateoas/src/main/java/com/example/GreetingController.java
@@ -0,0 +1,48 @@
+package com.example;
+
+import org.springframework.hateoas.ResourceSupport;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+@RestController
+class GreetingController {
+    @RequestMapping("/")
+    Greet greet() {
+        return new Greet("Hello World!");
+    }
+
+    @RequestMapping("/greeting")
+    @ResponseBody
+    public HttpEntity<Greet> greeting(@RequestParam(value = "name", required = false, defaultValue = "HATEOAS") String name) {
+        Greet greet = new Greet("Hello " + name);
+        greet.add(linkTo(methodOn(GreetingController.class).greeting(name)).withSelfRel());
+        return new ResponseEntity<Greet>(greet, HttpStatus.OK);
+    }
+}
+
+class Greet extends ResourceSupport {
+    private String message;
+
+    public Greet() {
+    }
+
+    public Greet(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
