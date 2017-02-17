164492e83e030f80700a4482b4ad0fcff9c262f5
Soonki Ji <soonki.ji@gmail.com>
2017-02-16 23:30:03 -0500

    initial

---
+++ chap2/ex27bootcustomer/README.md
@@ -0,0 +1,15 @@
+# example
+* hal browser
+* h2
+* jpa
+
+# hal endpoints
+* http://localhost:8080
+
+# jpa rest endpoints
+* http://localhost:8080/customers
+* http://localhost:8080/customers/1
+* http://localhost:8080/customers?page=0
+* http://localhost:8080/customers/search
+* http://localhost:8080/customers/search/findByName{?name}
+* http://localhost:8080/profile/customers
---
+++ chap2/ex27bootcustomer/src/main/java/com/example/Application.java
@@ -0,0 +1,76 @@
+package com.example;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Optional;
+
+@Entity
+class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String email;
+
+    Customer() {
+    }
+
+    public Customer(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
+@RepositoryRestResource
+interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByName(@Param("name") String name);
+}
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(CustomerRepository repo) {
+        return (evt) -> {
+            repo.save(new Customer("Harry", "harry@example.com"));
+            repo.save(new Customer("Sally", "sally@example.com"));
+        };
+    }
+}
