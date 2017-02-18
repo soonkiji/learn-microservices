524c9da432bf27f71e3e604198a0e77a366bcaf1
Soonki Ji <soonki.ji@gmail.com>
2017-02-18 18:46:18 -0500

    initial

---
+++ chap2/ex29actuatorcustomhealth/src/main/java/com/example/GreetingController.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    @RequestMapping("/")
+    Greet greet() {
+        return new Greet("Hello World!");
+    }
+}
+
+class Greet {
+    private String message;
+
+    public Greet() {
+    }
+
+    public Greet(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
