27380970e2bcf000c72d248620a520d9d51a05c2
Soonki Ji <soonki.ji@gmail.com>
2017-02-18 20:59:38 -0500

    Swagger.

---
+++ chap2/ex2Aswagger/src/main/java/com/example/Greet.java
@@ -0,0 +1,20 @@
+package com.example;
+
+class Greet {
+    private String message;
+
+    public Greet() {
+    }
+
+    public Greet(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ chap2/ex2Aswagger/src/main/java/com/example/GreetingController.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class GreetingController {
+    @RequestMapping("/")
+    Greet greet() {
+        return new Greet("Hello World!");
+    }
+}
+
