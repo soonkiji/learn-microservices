80ab5083cb41582fc11104d40cb17a4cc00c0ae8
Soonki Ji <soonki.ji@gmail.com>
2017-02-22 20:10:54 -0500

    Use Java 7

--- chap2/ex21bootrest/build-and-run.bat
+++ chap2/ex21bootrest/build-and-run.bat
@@ -1,5 +1,7 @@
 call mvn -DskipTests clean package
 docker build -t ex21bootrest:0.0.1 .
-docker run -d -p 8080:8080 ex21bootrest:0.0.1
-mysleep 10
-start http://192.168.99.100:8080
+echo docker run -it --rm --name ex21bootrest -p 8080:8080 ex21bootrest:0.0.1
+start docker run -it --rm --name ex21bootrest -p 8080:8080 ex21bootrest:0.0.1
+mysleep 20
+echo start http://%DOCKER_MACHINE_NAME%:8080
+start http://%DOCKER_MACHINE_NAME%:8080
