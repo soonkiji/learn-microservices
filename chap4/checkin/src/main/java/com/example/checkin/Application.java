052a3d1e39a25853efe5c4a66ca1f1dbd0f76efd
Soonki Ji <soonki.ji@gmail.com>
2017-03-09 00:15:28 -0500

    Added CommandLineRunner

--- chap4/checkin/src/main/java/com/example/checkin/Application.java
+++ chap4/checkin/src/main/java/com/example/checkin/Application.java
@@ -1,11 +1,19 @@
 package com.example.checkin;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Running with " + Arrays.toString(args));
     }
 }
